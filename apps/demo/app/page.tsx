e6cfa55e1b1f7a1bd773aa0482c57d4e1366a30c
Jonel Hatwell <jonel.hatwell@gmail.com>
2025-11-20 23:05:03 +0800

    first commit

---
+++ apps/demo/app/page.tsx
@@ -0,0 +1,11 @@
+
+import { Snake } from "@jonelhatwell/games";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Arcade Demo</h1>
+      <Snake />
+    </div>
+  );
+}
---
+++ packages/games/src/games/Snake/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Snake = () => {
+    return <div>🐍 Snake</div>;
+};
